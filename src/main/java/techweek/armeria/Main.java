c676863775e52bba5cc3e041af6f253de4f324dd
Ikhun Um <ih.pert@gmail.com>
2024-08-18 16:23:53 +0900

    1. Armeria server

--- src/main/java/techweek/armeria/Main.java
+++ src/main/java/techweek/armeria/Main.java
@@ -1,7 +1,20 @@
 package techweek.armeria;
 
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.Server;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Armeria!");
+        final Server server =
+                Server.builder()
+                      .http(8080)
+                      .service("/greeting/:name", (ctx, req) -> {
+                          final String name = ctx.pathParam("name");
+                          return HttpResponse.of("Hello, " + name + '!');
+                      })
+                      .build();
+
+        server.start().join();
+        server.closeOnJvmShutdown();
     }
 }
