8bcc905a0f563326928d23faaa78b0fd31b9afe0
Ikhun Um <ih.pert@gmail.com>
2024-09-04 11:01:43 +0900

    7. Decorator

--- src/main/java/techweek/armeria/Main.java
+++ src/main/java/techweek/armeria/Main.java
@@ -1,15 +1,27 @@
 package techweek.armeria;
 
+import java.util.concurrent.CompletableFuture;
+
+import com.linecorp.armeria.common.HttpHeaderNames;
+import com.linecorp.armeria.common.metric.MeterIdPrefixFunction;
+import com.linecorp.armeria.common.prometheus.PrometheusMeterRegistries;
+import com.linecorp.armeria.common.util.UnmodifiableFuture;
 import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.auth.AuthService;
 import com.linecorp.armeria.server.docs.DocService;
 import com.linecorp.armeria.server.grpc.GrpcService;
+import com.linecorp.armeria.server.metric.MetricCollectingService;
+import com.linecorp.armeria.server.prometheus.PrometheusExpositionService;
+import com.linecorp.armeria.server.throttling.ThrottlingService;
+import com.linecorp.armeria.server.throttling.ThrottlingStrategy;
 
 import io.grpc.stub.StreamObserver;
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 import techweek.armeria.Greeting.HelloReply;
 import techweek.armeria.Greeting.HelloRequest;
 import techweek.armeria.GreetingServiceGrpc.GreetingServiceImplBase;
 
-public class Main {
+public final class Main {
 
     public static void main(String[] args) {
         final GrpcService grpcService =
@@ -18,15 +30,36 @@ public class Main {
                            .enableHttpJsonTranscoding(true)
                            .enableUnframedRequests(true)
                            .build();
+
+        final PrometheusMeterRegistry meterRegistry = PrometheusMeterRegistries.newRegistry();
         final Server server =
                 Server.builder()
                       .http(8080)
                       .service(grpcService)
                       .serviceUnder("/docs", new DocService())
+                      .meterRegistry(meterRegistry)
+                      // Expose Prometheus metrics.
+                      .service("/internal/metrics",
+                               PrometheusExpositionService.of(meterRegistry.getPrometheusRegistry()))
+                      // Collect metrics.
+                      .decorator(MetricCollectingService.newDecorator(
+                              MeterIdPrefixFunction.ofDefault("armeria.server")))
+                      // Apply rate limiting.
+                      .decorator(ThrottlingService.newDecorator(
+                              ThrottlingStrategy.rateLimiting(100)))
+                      // Authenticate requests.
+                      .decorator(AuthService.newDecorator((ctx, req) -> {
+                          final String token = req.headers().get(HttpHeaderNames.AUTHORIZATION, "");
+                          return isValidToken(token);
+                      }))
                       .build();
 
         server.start().join();
         server.closeOnJvmShutdown();
+    }
+
+    private static CompletableFuture<Boolean> isValidToken(String token) {
+        return UnmodifiableFuture.completedFuture(true);
     }
 
     private static final class GreetingService extends GreetingServiceImplBase {
